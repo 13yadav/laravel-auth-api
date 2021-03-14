c7f8d4fe4d75d1e981fdb421cd2c8c99fc38cc93
Ranjit Yadav <yadavranjit521@gmail.com>
2021-03-14 17:02:26 +0530

    Added: Signup Api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return 'Login endpoint.';
+    }
+
+    public function signup(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        $user->save();
+
+        return response()->json([
+            "message" => "User registered successfully"
+        ], 201);
+    }
+
+    public function index()
+    {
+        return 'Hello World';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::namespace('Api')->group(function () {
+    Route::prefix('auth')->group(function() {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('signup', [AuthController::class, 'signup']);
+    });
+
+    Route::group([
+        'middleware'=>'auth:api'
+    ], function() {
+        Route::get('index', [AuthController::class, 'index']);
+    });
 });
